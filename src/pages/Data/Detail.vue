6a885c7408ac6fdd487a61e3d3a679d2a803da73
koorimizuw <koorimizuw@yahoo.co.jp>
2021-04-13 06:56:25 +0900

    update: detail page

--- src/pages/Data/Detail.vue
+++ src/pages/Data/Detail.vue
@@ -1,4 +1,150 @@
-<template></template>
+<template>
+  <h2>
+    フィルター<el-button
+      class="title-icon"
+      :icon="filterOpen ? `el-icon-arrow-up` : `el-icon-arrow-down`"
+      circle
+      @click="openFilter"
+    ></el-button>
+  </h2>
+  <div v-if="filterOpen">
+    <div class="subtitle">難易度</div>
+    <p>
+      <el-checkbox-group v-model="opts.diff">
+        <el-checkbox-button
+          v-for="opts in diffOptions"
+          :label="opts.value"
+          :key="opts"
+          >{{ opts.label }}
+        </el-checkbox-button>
+      </el-checkbox-group>
+    </p>
+    <div class="subtitle">レベル</div>
+    <p>
+      <el-checkbox-group v-model="opts.level">
+        <el-checkbox-button
+          v-for="opts in levelOptions"
+          :label="opts"
+          :key="opts"
+          >{{ opts }}
+        </el-checkbox-button>
+      </el-checkbox-group>
+    </p>
+    <div class="subtitle">スコアランク</div>
+    <p>
+      <el-checkbox-group v-model="opts.score">
+        <el-checkbox-button
+          v-for="opts in scoreRank"
+          :label="opts.value"
+          :key="opts"
+          >{{ opts.label }}
+        </el-checkbox-button>
+      </el-checkbox-group>
+    </p>
+    <div class="subtitle">クリアランプ</div>
+    <p>
+      <el-checkbox-group v-model="opts.ablamp">
+        <el-checkbox-button
+          v-for="opts in abLamp"
+          :label="opts.value"
+          :key="opts"
+          >{{ opts.label }}
+        </el-checkbox-button>
+      </el-checkbox-group>
+    </p>
+    <p>
+      <el-checkbox-group v-model="opts.fblamp">
+        <el-checkbox-button
+          v-for="opts in fbLamp"
+          :label="opts.value"
+          :key="opts"
+          >{{ opts.label }}
+        </el-checkbox-button>
+      </el-checkbox-group>
+    </p>
+  </div>
+  <el-table
+    :data="pageSlice"
+    ref="filterTable"
+    style="width: 100%"
+    :row-class-name="formatRowClass"
+  >
+    <el-table-column prop="music_name" label="楽曲名" width="360">
+      <template #default="scope">
+        <i class="el-icon-tickets"></i>
+        <span style="margin-left: 10px">{{ scope.row.music_name }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column prop="genre_name" label="カテゴリー" width="130" />
+    <el-table-column
+      prop="diff"
+      label="難易度"
+      width="90"
+      :formatter="foramtDiffName"
+    >
+      <template #default="scope">
+        <el-tag class="table-diff" size="medium">{{
+          foramtDiffName(scope.row)
+        }}</el-tag>
+      </template>
+    </el-table-column>
+    <el-table-column prop="level" label="譜面定数" width="60" />
+    <el-table-column label="FB" width="60">
+      <template #default="scope">
+        <el-tag v-if="scope.row.is_full_bell" class="fb-tag">FB</el-tag>
+      </template>
+    </el-table-column>
+    <el-table-column label="AB" width="60">
+      <template #default="scope">
+        <el-tag v-if="scope.row.is_all_break" class="ab-tag">AB</el-tag>
+        <el-tag v-if="scope.row.is_full_combo" class="fc-tag">FC</el-tag>
+      </template>
+    </el-table-column>
+    <el-table-column
+      prop="technical_score"
+      label="テクニカルスコア"
+      width="140"
+    >
+      <template #default="scope">
+        <i class="el-icon-video-play"></i>
+        <span style="margin-left: 10px">{{
+          scope.row.technical_score.toLocaleString()
+        }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column label="ランク" width="80">
+      <template #default="scope">
+        <span>{{ scope.row.rank }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column label="Rating" width="70">
+      <template #default="scope">
+        <span>{{
+          calcRating(scope.row.technical_score, scope.row.level)
+        }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column prop="battle_score" label="バトルスコア" width="140">
+      <template #default="scope">
+        <i class="el-icon-video-play"></i>
+        <span style="margin-left: 10px">{{
+          scope.row.battle_score.toLocaleString()
+        }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column prop="over_damage" label="オーバー ダメージ" width="90" />
+  </el-table>
+  <el-pagination
+    @size-change="handleSizeChange"
+    @current-change="handleCurrentChange"
+    :current-page="page"
+    :page-sizes="pageSizeOptions"
+    :page-size="pageSize"
+    layout="total, sizes, prev, pager, next, jumper"
+    :total="filtered.length"
+  >
+  </el-pagination>
+</template>
 
 <script setup lang="ts">
 import { ElLoading } from "element-plus";
@@ -6,6 +152,29 @@ import firebase from "firebase";
 import router from "@/router";
 import { onMounted, computed, ref, reactive } from "vue";
 import { useStore } from "vuex";
+import { foramtDiffName, formatRowClass, calcRating } from "./util";
+import { diffOptions, scoreRank, abLamp, fbLamp, levelOptions } from "./util";
+
+const page = ref(1);
+const pageSizeOptions = ref([50, 100, 200, 300, 400, 500]);
+const pageSize = ref(100);
+
+const handleSizeChange = (val) => {
+  pageSize.value = val;
+};
+
+const handleCurrentChange = (val) => {
+  page.value = val;
+};
+
+const filterOpen = ref(false);
+const opts = reactive({
+  diff: [2, 3, 10],
+  score: [],
+  ablamp: [],
+  fblamp: [],
+  level: [],
+});
 
 const store = useStore();
 
@@ -24,4 +193,113 @@ onMounted(async () => {
     router.push("/data");
   }
 });
+
+const openFilter = () => {
+  filterOpen.value = !filterOpen.value;
+};
+
+const filtered = computed(() => {
+  let items = [];
+  for (let item of detail.value) {
+    if (opts.diff.length !== 0 && !opts.diff.includes(item.diff)) {
+      continue;
+    }
+    if (opts.score.length !== 0 && !opts.score.includes(item.rank)) {
+      continue;
+    }
+    if (opts.ablamp.length !== 0 && !opts.ablamp.includes(item.ab_lamp)) {
+      continue;
+    }
+    if (opts.fblamp.length !== 0 && !opts.fblamp.includes(item.fb_lamp)) {
+      continue;
+    }
+    if (opts.level.length !== 0 && !opts.level.includes(item.level_name)) {
+      continue;
+    }
+    items.push(item);
+  }
+
+  return items;
+});
+
+const pageSlice = computed(() => {
+  const itemStart = (page.value - 1) * pageSize.value;
+  const itemEnd =
+    itemStart + pageSize.value > filtered.value.length
+      ? filtered.value.length - 1
+      : itemStart + pageSize.value;
+
+  return filtered.value.slice(itemStart, itemEnd);
+});
 </script>
+
+<style lang="scss">
+.table-diff {
+  color: #fff;
+}
+.el-table {
+  .basic .table-diff {
+    background: #81c784 !important;
+    border-color: #66bb6a !important;
+  }
+  .advanced .table-diff {
+    background: #ffb74d !important;
+    border-color: #ffa726 !important;
+  }
+  .expert .table-diff {
+    background: #f06292 !important;
+    border-color: #ec407a !important;
+  }
+  .master .table-diff {
+    background: #ba68c8 !important;
+    border-color: #ab47bc !important;
+  }
+  .lunatic .table-diff {
+    background: #777 !important;
+    border-color: #333 !important;
+  }
+}
+
+.ab-tag {
+  color: #fff;
+  background: #ffd54f !important;
+  border-color: #ffca28 !important;
+}
+
+.fc-tag {
+  color: #fff;
+  background: #ffeb3b !important;
+  border-color: #fdd835 !important;
+}
+
+.fb-tag {
+  color: #fff;
+  background: #80deea !important;
+  border-color: #4dd0e1 !important;
+}
+
+.table-expand {
+  font-size: 0;
+
+  label {
+    width: 150px;
+    color: #99a9bf;
+  }
+  .el-form-item {
+    margin-right: 0;
+    margin-bottom: 0;
+    width: 100%;
+  }
+}
+</style>
+
+<style lang="scss" scoped>
+.title {
+  &-icon {
+    margin-left: 20px;
+  }
+}
+.subtitle {
+  font-size: 1.35em;
+}
+</style>
--- src/pages/Data/util.ts
+++ src/pages/Data/util.ts
@@ -1,60 +1,103 @@
-import firebase from "firebase";
-
 export const diffOptions = [
-	{ label: "Basic", value: 0 },
-	{ label: "Advanced", value: 1 },
-	{ label: "Expert", value: 2 },
-	{ label: "Master", value: 3 },
-	{ label: "Lunatic", value: 10 },
+  { label: "Basic", value: 0 },
+  { label: "Advanced", value: 1 },
+  { label: "Expert", value: 2 },
+  { label: "Master", value: 3 },
+  { label: "Lunatic", value: 10 },
 ]
+
 export const scoreRank = [
-	{ label: "-", value: null },
-	{ label: "A未満", value: "" },
-	{ label: "A", value: "A" },
-	{ label: "AA", value: "AA" },
-	{ label: "AAA", value: "AAA" },
-	{ label: "S", value: "S" },
-	{ label: "SS", value: "SS" },
-	{ label: "SSS", value: "SSS" },
-	{ label: "SSS+", value: "SSS+" },
-	{ label: "MAX", value: "MAX" },
+  { label: "-", value: null },
+  { label: "A未満", value: "" },
+  { label: "A", value: "A" },
+  { label: "AA", value: "AA" },
+  { label: "AAA", value: "AAA" },
+  { label: "S", value: "S" },
+  { label: "SS", value: "SS" },
+  { label: "SSS", value: "SSS" },
+  { label: "SSS+", value: "SSS+" },
+  { label: "MAX", value: "MAX" },
 ];
+
 export const abLamp = [
-	{ label: "ノーランプ", value: null },
-	{ label: "AB", value: "AB" },
-	{ label: "FC", value: "FC" },
+  { label: "ノーランプ", value: null },
+  { label: "AB", value: "AB" },
+  { label: "FC", value: "FC" },
 ];
+
 export const fbLamp = [
-	{ label: "ノーランプ", value: null },
-	{ label: "FB", value: "FB" },
+  { label: "ノーランプ", value: null },
+  { label: "FB", value: "FB" },
+];
+
+export const levelOptions = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "7+",
+  "8",
+  "8+",
+  "9",
+  "9+",
+  "10",
+  "10+",
+  "11",
+  "11+",
+  "12",
+  "12+",
+  "13",
+  "13+",
+  "14",
+  "14+",
 ];
 
 export const foramtDiffName = (row) => {
-	switch (row.diff) {
-		case 0:
-			return "Basic";
-		case 1:
-			return "Advanced";
-		case 2:
-			return "Expert";
-		case 3:
-			return "Master";
-		case 10:
-			return "Lunatic";
-	}
+  switch (row.diff) {
+    case 0:
+      return "Basic";
+    case 1:
+      return "Advanced";
+    case 2:
+      return "Expert";
+    case 3:
+      return "Master";
+    case 10:
+      return "Lunatic";
+  }
 };
 
 export const formatRowClass = ({ row }) => {
-	switch (row.diff) {
-		case 0:
-			return "basic";
-		case 1:
-			return "advanced";
-		case 2:
-			return "expert";
-		case 3:
-			return "master";
-		case 10:
-			return "lunatic";
-	}
+  switch (row.diff) {
+    case 0:
+      return "basic";
+    case 1:
+      return "advanced";
+    case 2:
+      return "expert";
+    case 3:
+      return "master";
+    case 10:
+      return "lunatic";
+  }
+}
+
+export const calcRating = (score: number, level: number): number => {
+  let rt = 0
+  if (score >= 1007500) {
+    rt = level + 2.0
+  } else if (score >= 1000000) {
+    const rmrt = Math.floor((score - 1000000) / 150) / 100
+    rt = level + 1.5 + rmrt
+  } else if (score >= 970000) {
+    const rmrt = Math.floor((score - 970000) / 200) / 100
+    rt = level + rmrt
+  } else {
+    const rmrt = Math.floor((970000 - score) / 175 + 1) / 100
+    rt = level - rmrt
+  }
+  return Math.round(rt * 100) / 100
 }
