007fdbbecb061bb89ff1fd9c96b9249ab36592c1
rainy-me <github@rainy.me>
2021-04-13 14:07:45 +0900

    feat: allow emulator

---
+++ src/utils.ts
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+export function getFunctions() {
+  const app = firebase.app();
+  if (window.location.host.includes("localhost")) {
+    firebase.functions().useEmulator("localhost", 5001);
+    return app.functions(); // for emulator
+  }
+  return app.functions("asia-northeast1"); // for prod
+}
